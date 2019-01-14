56b10ecc7fb2672a5fb496ccd4c95ecb9c175531
Bishnu Pokhrel <bishnup212@gmail.com>
2019-01-14 15:18:02 +0545

    Product commiting resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources\Product;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,8 +16,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name'=>$this->name,
+            'Total_price' => round((1 - ($this->discount) / 100) * $this->price,2),
+            'rating'=>$this->review->count()>0 ?round($this->review->sum('star')/$this->review->count(),2):'No rating yet',
+            'discount'=>$this->discount,
+            'href' => [
+                'link' => route('product.show', $this->id)
+            ],
+        ];
     }
-
-
 }
