2465a98992b5ee84f4ce24af80f21b08bfde7ff8
Bishnu Pokhrel <bishnup212@gmail.com>
2019-01-14 11:46:32 +0545

    ading faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail'=>$faker->paragraph,
+        'stock'=>$faker->randomDigit,
+        'price' => $faker->numberBetween(100, 1000),
+        'discount'=>$faker->numberBetween(2,30),
     ];
 });
