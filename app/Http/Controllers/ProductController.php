ec9df15275c26b6fef6e1e4b7aca3c5b04e5d106
Bishnu Pokhrel <bishnup212@gmail.com>
2019-01-15 11:44:20 +0545

    product adding and commiting

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -38,11 +45,22 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(productRequest $request)
     {
 
+//        return "helllo";
+//        return $request->all();
 
-
+        $product = new Product;
+        $product->name = $request->name;
+        $product->detail=$request->description;
+        $product->stock = $request->stock;
+        $product->price=$request->price;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+            'data'=>new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -77,7 +95,16 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $request['detail']=$request->description;
+        unset($request['description']);
+        $product->update($request->all());
+
+        if ($product) {
+            return response([
+                'data'=>new ProductResource($product)
+            ],Response::HTTP_CREATED);
+
+        }
     }
 
     /**
