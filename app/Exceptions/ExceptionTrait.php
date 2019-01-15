8d9ba3d20fa918ce480a3898d7d88043e84c2d2e
Bishnu Pokhrel <bishnup212@gmail.com>
2019-01-15 12:32:22 +0545

    customized exception trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bishnu pokhrel
+ * Date: 1/15/2019
+ * Time: 12:10 PM
+ */
+
+namespace App\Exceptions;
+
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+trait ExceptionTrait
+{
+    public function apiException($request,$e)
+    {
+        if ($this->isModel($e)) {
+           return $this->modelResponse();
+
+        }
+        if ($this->isHttp($e)) {
+           return $this->httpResponse();
+        }
+    }
+
+    protected function isHttp($e)
+    {
+        return $e instanceof NotFoundHttpException;
+    }
+
+    Protected function isModel($e)
+    {
+        return $e instanceof ModelNotFoundException ;
+
+    }
+
+    protected function modelResponse()
+    {
+        return response()->json([
+            'errors'=>'Product model not found'
+        ],Response::HTTP_NOT_FOUND);
+    }
+
+    protected function httpResponse()
+    {
+        return response()->json([
+            'errors'=>'Incorrect Route'
+        ],Response::HTTP_NOT_FOUND);
+
+    }
+
+}
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,16 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
+
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -46,6 +52,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()) {
+            return $this->apiException($request,$exception);
+
+
+        }
+
         return parent::render($request, $exception);
     }
 }
